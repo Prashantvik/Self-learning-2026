98c9388a34da51a1dc4bdb751b9745a919b5641a
prashantvikram <prashant.vikram@reddoorz.com>
2026-02-07 22:44:03 +0530

    Fix formatting

--- Python/Projects/Fraction data type/Fraction.py
+++ Python/Projects/Fraction data type/Fraction.py
@@ -11,12 +11,14 @@ class Fraction:
 
     # Dunder/Magic method implementation
     def __add__(self, other):
-        new_numerator = self.numerator * other.denominator + other.numerator * self.denominator
+        new_numerator = self.numerator * other.denominator 
+        + other.numerator * self.denominator
         new_denominator = self.denominator * other.denominator
         return Fraction(new_numerator, new_denominator)
 
     def __sub__(self, other):
-        new_numerator = self.numerator * other.denominator - other.numerator * self.denominator
+        new_numerator = self.numerator * other.denominator 
+        - other.numerator * self.denominator
         new_denominator = self.denominator * other.denominator
         return Fraction(new_numerator, new_denominator)
 
